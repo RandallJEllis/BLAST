1262b576c3e8b71c5125664aa81c7c721e4a0637
RandallJEllis <randalljellis@gmail.com>
2016-02-17 11:36:58 -0500

    Ident calculation change
    Instead of dividing the identity value (hsp.identities) over the queried
    protein length, it now divides over the specific alignment length
    (hsp.align_length)

--- entrez_blast_homology.py
+++ entrez_blast_homology.py
@@ -8,6 +8,7 @@ from Bio import Entrez
 from Bio.Blast.Applications import NcbiblastpCommandline
 from Bio.Blast import NCBIXML
 import csv
+import time
 
 Entrez.email = "randalljellis@gmail.com"
 Tool = "SoularBLASTer"
@@ -18,25 +19,31 @@ homologies = []
 for gene in genes:
     handle = Entrez.esearch(db="protein", term="Mus musculus[Orgn] AND " + gene + "[Gene]")
     record = Entrez.read(handle)
-    get_fasta = Entrez.efetch(db="protein", id=record["IdList"][0], rettype="fasta", retmode="text")
-    filename = record["IdList"][0] + '.fasta'
-    out_handle = open(filename, "w")
-    out_handle.write(get_fasta.read())
-    out_handle.close()
-    get_fasta.close()
-    
-    blastp_cline = NcbiblastpCommandline(query=filename, db="nr", evalue=0.001, outfmt=5, out=filename[:-5] + ".xml", num_alignments=1, entrez_query="human[Organism]", remote=True)
-    
-    stdout, stderr = blastp_cline()
-    
-    blastp_cline = open(filename[:-5] + ".xml")
-    blast_record = NCBIXML.read(blastp_cline)
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            ident = float(hsp.identities)/alignment.length
+    if len(record["IdList"]) > 0:
+        get_fasta = Entrez.efetch(db="protein", id=record["IdList"][0], rettype="fasta", retmode="text")
+        filename = record["IdList"][0] + '.fasta'
+        out_handle = open(filename, "w")
+        out_handle.write(get_fasta.read())
+        out_handle.close()
+        get_fasta.close()
+        
+        blastp_cline = NcbiblastpCommandline(query=filename, db="nr", evalue=0.001, outfmt=5, out=filename[:-5] + ".xml", num_alignments=1, entrez_query="human[Organism]", remote=True)
+        
+        stdout, stderr = blastp_cline()
+        
+        blastp_cline = open(filename[:-5] + ".xml")
+        blast_record = NCBIXML.read(blastp_cline)
+        for alignment in blast_record.alignments:
+            hsp = alignment.hsps[0]
+            ident = float(hsp.identities)/hsp.align_length
             accession = alignment.accession
             accession_numbers.append(accession)
             homologies.append(ident)
+            time.sleep(1)
+    else:
+        accession_numbers.append("0")
+        homologies.append("0")
+        continue
     
 rows = zip(genes, accession_numbers, homologies)
 with open('homologies.csv', 'wb') as thefile:
