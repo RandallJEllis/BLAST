9c8091b6d5afdb90ed9314af5c2253e6b5a7d24f
RandallJEllis <randalljellis@gmail.com>
2016-02-17 16:02:18 -0500

    Queried gene accession numbers
    Now there are four accumulating lists in the script: the queried gene,
    the queried gene's accession number, the top homolog's accession number,
    and the top homolog's ident value.

--- entrez_blast_homology.py
+++ entrez_blast_homology.py
@@ -14,12 +14,16 @@ Entrez.email = "randalljellis@gmail.com"
 Tool = "SoularBLASTer"
 
 genes = [gene.rstrip('\n') for gene in open('genes.txt')]
+query_accession_numbers = []
 accession_numbers = []
 homologies = []
+
 for gene in genes:
     handle = Entrez.esearch(db="protein", term="Mus musculus[Orgn] AND " + gene + "[Gene]")
     record = Entrez.read(handle)
+    
     if len(record["IdList"]) > 0:
+        query_accession_numbers.append(record["IdList"][0])
         get_fasta = Entrez.efetch(db="protein", id=record["IdList"][0], rettype="fasta", retmode="text")
         filename = record["IdList"][0] + '.fasta'
         out_handle = open(filename, "w")
@@ -46,17 +50,16 @@ for gene in genes:
             homologies.append("No homologs")
             time.sleep(1)
             continue
-
     else:
         accession_numbers.append("No FASTA seq")
         homologies.append("No FASTA seq")
         time.sleep(1)
         continue
     
-rows = zip(genes, accession_numbers, homologies)
+rows = zip(genes, query_accession_numbers, accession_numbers, homologies)
 with open('homologies.csv', 'wb') as thefile:
     writer = csv.writer(thefile)
-    writer.writerow(['Gene', 'Homolog Accession', 'Homology percentage'])
+    writer.writerow(['Gene', 'Gene Accession', 'Homolog Accession', 'Homology percentage'])
     for row in rows:
         writer.writerow(row)    
     
